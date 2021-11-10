d7c055d53253cda5bf212a3a9b2193ae4c000b53
RaquelziinhaRaquelziinha <kekinhaspssos@gmail.com>
2021-11-10 20:20:37 -0300

    fazendo commit

---
+++ AULA12/HTML_CSS-JS/JS/AULA02/exercicios/main.js
@@ -0,0 +1,14 @@
+!<DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="shortcut icon" href="#" />
+        <title>Strings</title>
+    </head>
+    <body>
+        <script src="./js/arrays.js"></script>
+    </body>
+</html>
+
+console user list =[
+{name:'Amanda',}
